9175f9d4fc390e292d719157544d6f32d73a525c
ikeola emmanuel <emmanuel.ikeola@student.griffith.ie>
2021-02-13 23:47:08 +0000

    first init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AskComponent } from './ask/ask.component';
+import { CongratsComponent } from './congrats/congrats.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'ask', pathMatch: 'full' },
+  { path: 'ask', component: AskComponent },
+  { path: 'congrats', component:  CongratsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CongratsComponent } from './congrats/congrats.component';
+import { AskComponent } from './ask/ask.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CongratsComponent,
+    AskComponent
   ],
   imports: [
     BrowserModule,
